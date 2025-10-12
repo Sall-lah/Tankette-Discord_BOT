fed99bf188f29f5dedeb849ff0d09c48c021fb3a
Sall-lah <ikhsan.ibnu.abdulah@gmail.com>
2025-10-12 22:04:49 +0700

    Updated function to a promise

--- src/event/interactionCreate/gameFiles/game.js
+++ src/event/interactionCreate/gameFiles/game.js
@@ -8,23 +8,43 @@ module.exports = async (client, interaction, playerIds) => {
     const rollNumber = Math.floor(Math.random() * totalPlayer);
 
     // Assign Killer and Crew
-    const player = {
+    let player = {
         killer: playerIds.filter(id => id == playerIds[rollNumber]),
         crew: playerIds.filter(id => id != playerIds[rollNumber]),
+        dead: []
     }
 
     // while (player.killer.length > 0 && player.crew.length > 1) {
     while(true) {
-        // Create Story Embeds
+        // Send a night dialog
         const embedNight = new EmbedBuilder()
             .setTitle("At Night")
             .setDescription("The Traitor is Planning to do something tonight");
-
         await interaction.channel.send({
             embeds : [embedNight]
         })
 
+        // Get the user that is chosed by the traitor
         const killResult = await traitorMove(client, interaction, player);
+
+        // Create and Send Result message
+        let embedNightResult;
+        if(killResult.length > 0){
+            // Change player status
+            player.crew = player.crew.filter((t) => {return t !== killResult[0].m.values[0]});
+            player.dead.push(killResult[0].m.values[0]);
+            
+            embedNightResult = new EmbedBuilder()
+            .setTitle("In the morning")
+            .setDescription(`Last night, a crew with the name of <@${killResult[0].m.values[0]}> have been killed by a traitor`);
+        } else {
+            embedNightResult = new EmbedBuilder()
+            .setTitle("In the morning")
+            .setDescription("No one is dead today");
+        }
+        await interaction.channel.send({
+            embeds : [embedNightResult]
+        })
 
         // Create Story Embeds
         // const embedNoon = new EmbedBuilder()
--- src/event/interactionCreate/gameFiles/traitorMove.js
+++ src/event/interactionCreate/gameFiles/traitorMove.js
@@ -1,65 +1,63 @@
 const { EmbedBuilder, ActionRowBuilder, UserSelectMenuBuilder } = require('discord.js');
 
-// Note for future Ikhsan: Make ts a promise vro
-// const promises = new promises((resolve, reject) => {
-
-// })
-
 module.exports = (async (client, interaction, player) => {
-    const embeds = new EmbedBuilder()
-        .setDescription("What will you do");
-    // Create User Select Menu
-    let userMenu = new UserSelectMenuBuilder()
-        .setCustomId('userSelect')
-        .setPlaceholder('Choose a user')
-        .setMinValues(1)
-        .setMaxValues(1);
+    return new Promise(async (resolve) => {
+        const embeds = new EmbedBuilder()
+            .setDescription("What will you do");
+        // Create User Select Menu
+        let userMenu = new UserSelectMenuBuilder()
+            .setCustomId('userSelect')
+            .setPlaceholder('Choose a user')
+            .setMinValues(1)
+            .setMaxValues(1);
 
-    let row = new ActionRowBuilder().addComponents(userMenu);
+        let row = new ActionRowBuilder().addComponents(userMenu);
 
-    const msg = await interaction.channel.send({
-        embeds: [embeds],
-        components: [row]
-    });
-
-    const collector = msg.createMessageComponentCollector({ time: 15000 });
-
-    const victim = []
-
-    await collector.on('collect', async (m) => {
-        console.log(player);
-        await m.deferReply({
-            flags: 64, // <- only the user that make the interaction can see
+        const msg = await interaction.channel.send({
+            embeds: [embeds],
+            components: [row]
         });
 
-        if (m.customId === "userSelect") {
-            if (player.killer.includes(m.user.id)) {
-                victim.push({ m });
-                await m.editReply({
-                    content: "KYS vro"
-                })
-            }
-            else if (player.crew.includes(m.user.id)){
-                await m.editReply({
-                    content: "You are not a Traitor"
-                })
+        const collector = msg.createMessageComponentCollector({ time: 15000 });
+
+        const victim = []
+
+        await collector.on('collect', async (m) => {
+            console.log(player);
+            await m.deferReply({
+                flags: 64, // <- only the user that make the interaction can see
+            });
+
+            if (m.customId === "userSelect") {
+                if (player.killer.includes(m.user.id)) {
+                    victim.push({ m });
+                    await m.editReply({
+                        content: "KYS vro"
+                    })
+                }
+                else if (player.crew.includes(m.user.id)) {
+                    await m.editReply({
+                        content: "You are not a Traitor"
+                    })
+                }
+                else {
+                    await m.editReply({
+                        content: "You havent joined this session of the game"
+                    })
+                }
             }
             else {
-                await m.editReply({
-                    content: "You havent joined this session of the game"
-                })
+
             }
-        }
-        else {
-            
-        }
-    });
+        });
 
-    await collector.on('end', () => {
-        interaction.channel.send("The vote has ended");
-        return victim;
-    });
-
-    // Set timeout so that the while loop stop until the collecter ended
-    await new Promise(resolve => setTimeout(resolve, 15000));
-})
+        await collector.on('end', async() => {
+            const embed = new EmbedBuilder()
+                .setDescription("The voting has ended");
+            await interaction.channel.send({ embeds: [embed]});
+            resolve(victim);
+        });
+        // Set timeout so that the while loop stop until the collecter ended
+        await new Promise(resolve => setTimeout(resolve, 15000));
+    })
+});
